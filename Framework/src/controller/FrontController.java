61d33f4bf8ca90aff7610d07ae6961022f1ccf60
Kiady <kiadyandriamanantsilavo@gmail.com>
2024-05-07 11:57:08 +0300

    sprint0 finished

---
+++ Framework/src/controller/FrontController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.util.*;
+
+public class FrontController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Toutes url dans FrontController");
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+}
