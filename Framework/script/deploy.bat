0b8ecaaa8472dd4b9fd73b1df13b7c4abdbfd47f
Kiady <kiadyandriamanantsilavo@gmail.com>
2024-05-17 10:39:33 +0300

    Validation de sprint1
    Test réussi pour toutes les classes annotées controlleurs
    
    *** Contraintes : les classes sans package ne sont pas considérées

--- Framework/script/deploy.bat
+++ Framework/script/deploy.bat
@@ -9,13 +9,6 @@ set "libPath=..\lib\"
 set "binPath=..\bin\"
 set "tempJava=..\tempJava\"
 set "testPath=D:\ITU\S4\Web_Dynamique(Mr_Naina)\mySpring_Framework\Test\"
-set "webappsPath=D:\ITU\Server\apache-tomcat-10.1.7\webapps\"
-
-rem suppression de test
-rmdir /s /q "%testPath%"
-
-rem re-création de test
-mkdir "%testPath%"
 
 rem création de tempJava et binPath
 mkdir "%tempJava%"
@@ -30,27 +23,13 @@ for /r "%srcPath%" %%f in (*.java) do (
 rem compilation des src java vers bin
 javac -cp %libPath%* -d %binPath% ..\tempJava\*.java
 
-rem architecture de Test
-mkdir "%testPath%\WEB-INF"
-mkdir "%testPath%\WEB-INF\lib"
-
 rem archivage en jar du contenu de bin
 pushd "%binPath%"
 jar cf "%jarName%.jar" *
 popd
 
 rem copie de jar vers lib de test Path
-xcopy "%binPath%\%jarName%.jar" %testPath%\WEB-INF\lib /Y
+xcopy "%binPath%\%jarName%.jar" %testPath%\lib /Y
 
 rem suppression de tempJava 
 rmdir /s /q "%tempJava%"
-
-rem suppression de binPath
-rmdir /s /q "%binPath%"
-
-rem archivage du contenu de Test
-pushd "%testPath%"
-jar cf "%appName%.war" *
-popd
-
-xcopy "%testPath%\%appName%.war" %webappsPath% /Y
---
+++ Framework/src/controller/ClassesScanneur.java
@@ -0,0 +1,75 @@
+package controlleur;
+
+import java.net.URL;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.util.*;
+import annotation.Controlleur;
+import util.Util;
+
+public class ClassesScanneur extends HttpServlet {
+
+    private String basePackage;
+    boolean checked = false;
+    List<Class<?>> founded;
+
+    public List<Class<?>> getFounded() {
+        return founded;
+    }
+
+    public void setFounded(List<Class<?>> founded) {
+        this.founded = founded;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    void initVariables() {
+        String base = this.getInitParameter("base-package");
+        try {
+            basePackage = base;
+            this.setFounded(new Util().getClassesByAnnotation(this.basePackage, Controlleur.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() {
+        initVariables();
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        if (!this.isChecked()) { // haven't initalized variables yet
+            // out.print("first time called this servlet");
+            initVariables();
+            this.setChecked(true);
+        }
+        try {
+            out.println("Liste de  tous les controlleurs du projet actuel : ");
+            List<Class<?>> classes = this.getFounded();
+            for (int i = 0; i < classes.size(); i++) {
+                out.println(classes.get(i).getName());
+            }
+        } catch (Exception e) {
+            out.println(e.getMessage());
+        }
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+}
